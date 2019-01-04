5f38e0f3ec002f44c8f723a45ad7c96982406dd1
Alex Vance <herdadvance@gmail.com>
2019-01-04 16:41:22 -0500

    First

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="UTF-8">
+    <title>Follege Cootball</title>
+    <link rel="icon" href="favicon.ico">
+
+    <meta name="author" content="Bulldog Creative Services">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+    <link href="/assets/css/normalize.css" rel="stylesheet">
+    <link href="/assets/css/style.css?v=<?php echo date('U');?>" rel="stylesheet">
+    <link href="/assets/css/mobile.css?v=<?php echo date('U');?>" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,600,700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body class="home">
+
+  	<header class="header wrap-bg flex">
+  		<a href="/"><!--img class="logo" src="/assets/images/coon-logo.png" alt="Logo"--></a>
+        <div class="header-text">
+            <h1 class="no-mar-bot white-text">Follege Cootball</h1>
+            <h2 class="no-mar-bot sec-text superlight">The nation's dumbest college football site</h2>
+        </div>
+  	</header>
+
+    <nav class="nav nav-inline nav-stretch wrap-bg pri shadow on-top">
+        <ul>
+            <li><a href="sub.php">Rankings</a></li>
+            <li><a href="sub.php">Conferences</a>
+                <ul>
+                    <li><a href="">ACC</a></li>
+                    <li><a href="">American</a></li>
+                    <li><a href="">Big 10</a></li>
+                    <li><a href="">Big 12</a></li>
+                    <li><a href="">C-USA</a></li>
+                    <li><a href="">MAC</a></li>
+                    <li><a href="">MWC</a></li>
+                    <li><a href="">Pac-12</a></li>
+                    <li><a href="">SEC</a></li>
+                    <li><a href="">SunBelt</a></li>
+                </ul>
+            </li>
+            <li><a href="sub.php">Something Else</a></li>
+            <li><a href="sub.php">Podcast</a></li>
+        </ul>
+    </nav>
+  	
+  	<main>
+	  	
+        <section class="splash wrap-bg rel flex" style="background-image: url(/assets/images/psu.jpg);">
+            <div class="on-top">
+                <h1 class="no-mar-bot">Welcome</h1>
+            </div>
+            <div class="overlay"></div>
+        </section>
+
+        <section class="wrap">
+            <h1>Don't Sort Through the Dumpster for a Good Framework</h1>
+            <p>You told me two pounds and now you waste my time with these Chiclets? Seventeen and a half, minus the half for wasting my time. What, you gonna argue? You got something to say? You're doing business like a couple little bitches.</p> 
+
+            <p>What did you say? Look old bald motherfucker. Fifty-two and a half, twenty-five points dig. No... two pounds next Friday and no production problems. Listen old man, talk is talk, but owing me money? That's bad.</p>
+
+            <p>What did I say, man, this guy can COOK! You're all right, you're all right man... we're gonna make a looot of money together. What did you say? Like they don't already know that? Are you saying that they're stupid? Okay, so you're not saying that they're stupid. So I don't understand are you saying that I'm stupid?</p>
+
+            <p>No, you're just speaking for me! Like I ain't got the goddamn sense to speak for myself! Is that it? Is that what you're doing? Heisenberg says, 'Relax'!</p>
+        </section>
+
+        <section class="more-coon wrap-bg sec shadow">
+            <div class="row">
+                 <div class="col-4">
+                    <h1 class="centered">Famous Raccoons</h1>
+                    <img src="/assets/images/rocket-raccoon.jpg" alt="Description">
+                    <p>Roll over and sun my belly attack dog, run away and pretend to be victim or ignore the squirrels, you'll never catch them anyway terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry i cry and cry and cry unless you pet me, and then maybe i cry just for fun or instantly break out into full speed gallop across the house for no reason, and eat a rug and furry furry hairs everywhere oh no human coming.</p>
+                </div>
+                <div class="col-4">
+                    <h1 class="centered">Home Sweet Home</h1>
+                    <img src="/assets/images/dumpster.jpg" alt="Description">
+                    <p>Meow for food, then when human fills food dish, take a few bites of food and continue meowing cough hairball on conveniently placed pants yet groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked going to catch the red dot today going to catch the red dot today, somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock or lick plastic bags.</p>
+                </div>
+                <div class="col-4">
+                    <h1 class="centered">Finding Food</h1>
+                    <img src="/assets/images/coon-birdfeeder.jpg" alt="Description">
+                    <p>Chase laser. Love and coo around boyfriend who purrs and makes the perfect moonlight eyes so i can purr and swat the glittery gleaming yarn to him (the yarn is from a $125 sweater) lick yarn hanging out of own butt or give me attention or face the wrath of my claws stare at wall turn and meow stare at wall some more meow again continue staring so leave fur on owners clothes.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="wrap">
+            <h1 class="centered">Join the Coon Mailing List</h1>
+            <form action="/sub.php">
+                <label for="name">Name</label>
+                <input name="name" id="name" type="text">
+                
+                <label for="email">E-Mail</label>
+                <input name="email" id="email" type="text">
+
+                <label for="food">Favorite Dumpster Food</label>
+                <select name="food" id="food">
+                    <option value="" disabled selected>---Select A Food---</option>
+                    <option value="">Banana Peels</option>
+                    <option value="">Apple Cores</option>
+                    <option value="">Cat Food</option>
+                    <option value="">Vegetables</option>
+                    <option value="">Raw Meat</option>
+                </select>
+
+                <label for="more">Tell Us More About Yourself</label>
+                <textarea name="more" id="more"></textarea>
+
+                <button class="btn" type="submit">Sign Up Now!</button>
+            </form>
+        </section>
+
+  	</main>
+
+  	<footer class="footer wrap-bg centered">
+  		<p class="no-mar-y sec-text">&copy; <?php echo date('Y'); ?> Property of Coon</p>
+  	</footer>
+
+  	<script src="/assets/js/script.js"></script>
+
+  </body>
+
+</html>
